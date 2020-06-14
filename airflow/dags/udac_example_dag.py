e59878988200f81d2e4029584d06cb4ef02c3da6
John Sinues <walkandride@hotmail.com>
2020-06-14 19:30:16 -0400

    feat: configure DAG
    In the DAG, add default parameters according to these guidelines
    
    - The DAG does not have dependencies on past runs
    - On failure, the task are retried 3 times
    - Retries happen every 5 minutes
    - Catchup is turned off
    - Do not email on retry
    
    Define task dependencies as
                                       load song_dim table
    stage events                       load user_dim table
                -> load songplays ->   load artist_dim table  -> run data quality checks
    stage songs                        load time_dim table

--- airflow/dags/udac_example_dag.py
+++ airflow/dags/udac_example_dag.py
@@ -12,29 +12,24 @@ from helpers import SqlQueries
 default_args = {
     'owner': 'udacity',
     'start_date': datetime(2019, 1, 12),
-    'depends_on_past': False,
-    'retries': 3,
-    'retry_delay': timedelta(minutes=5),
-    'email_on_retry': False
 }
 
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *',
-          catchup=False
+          schedule_interval='0 * * * *'
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
 )
 
 load_songplays_table = LoadFactOperator(
@@ -68,7 +63,3 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
-
-stage_events_to_redshift >> load_songplays_table
-stage_songs_to_redshift >> load_songplays_table
-load_songplays_table >> [load_song_dimension_table, load_user_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks
