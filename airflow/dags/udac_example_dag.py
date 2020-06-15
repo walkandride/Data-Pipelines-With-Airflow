523c49660e510ce56b389dc5e6061568253b835a
John Sinues <walkandride@hotmail.com>
2020-06-14 23:04:53 -0400

    feat: configure stage_XXXX_to_redshift
    Added create_tables_task.  Add prelim
    code to stage_events_to_redshift and
    stage_songs_to_redshift.  Updated
    task dependencies.

--- airflow/dags/udac_example_dag.py
+++ airflow/dags/udac_example_dag.py
@@ -27,14 +27,34 @@ dag = DAG('udac_example_dag',
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
+create_tables_task = PostgresOperator(
+    task_id="create_tables",
+    dag=dag,
+    sql='create_tables.sql',
+    postgres_conn_id="redshift"
+)
+
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
+    aws_credential_id='aws_credentials',
+    redshift_conn_id='redshift',
+    table_name='staging_events',
+    s3_bucket='udacity-dend',
+    s3_key='log_data/2018/11/2018-11-01-events.json', # "log_data/{{ execution_date.strftime('%Y') }}/{{ execution_date.strftime('%m') }}/{{ ds }}-events.json"
+    log_json_file='log_json_path.json',
+    provide_context=True
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    aws_credential_id='aws_credentials',
+    redshift_conn_id='redshift',
+    table_name='staging_songs',
+    s3_bucket='udacity-dend',
+    s3_key='song_data/A/A/*/*.json',
+    provide_context=True
 )
 
 load_songplays_table = LoadFactOperator(
@@ -69,6 +89,4 @@ run_quality_checks = DataQualityOperator(
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
-stage_events_to_redshift >> load_songplays_table
-stage_songs_to_redshift >> load_songplays_table
-load_songplays_table >> [load_song_dimension_table, load_user_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks
+start_operator >> create_tables_task >> [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table >> [load_song_dimension_table, load_user_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks >> end_operator
